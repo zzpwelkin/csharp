d6aa5097646be651ac55611e1e7563d8178f4028
zzpwelkin <zzpwelkin@163.com>
2018-02-26 15:38:47 +0800

    从大的软件工程方面开始介绍课程

---
+++ 2_Ccharp语言学习导引.markdown
@@ -0,0 +1,23 @@
+### [.Net平台](https://docs.microsoft.com/zh-cn/dotnet/framework/get-started/overview)和C#语言介绍
+	
+* 要求 
+
+	1. 学生已提前安装了Visual Studio并自己对其有了初步的尝试;
+	
+	2. UML建模语言了解及相关软件已安装
+
+* 本课内容
+
+	1. .NET平台介绍
+
+	2. C#语言特性介绍
+
+	3. 第一个简单的C#程序
+
+	4. CRS存取一体机系统*用例*分析
+		
+		参与者、系统边界、功能模块
+
+* 实践
+
+		1. CRS系统 __打印__ 模块
---
+++ 软件世界.mm
@@ -0,0 +1,76 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x65b0;&#x5efa;&#x601d;&#x7ef4;&#x5bfc;&#x56fe;" FOLDED="false" ID="ID_1816815465" CREATED="1519529554378" MODIFIED="1519529785173" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="&#x8f6f;&#x4ef6;" POSITION="right" ID="ID_135133580" CREATED="1519529757110" MODIFIED="1519530354808" LINK="https://en.wikipedia.org/wiki/Software" HGAP_QUANTITY="57.49999870359901 pt" VSHIFT_QUANTITY="2.2499999329447764 pt">
+<edge COLOR="#ff0000"/>
+<attribute NAME="&#x94fe;&#x63a5;" VALUE="https://en.wikipedia.org/wiki/Software" OBJECT="java.net.URI|https://en.wikipedia.org/wiki/Software"/>
+<hook URI="software.PNG" SIZE="0.98220354" NAME="ExternalObject"/>
+<node TEXT="&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;" ID="ID_1614087147" CREATED="1519529787424" MODIFIED="1519530115144" LINK="https://en.wikipedia.org/wiki/Computer_system"/>
+<node TEXT="&#x5e94;&#x7528;&#x8f6f;&#x4ef6;" ID="ID_565795833" CREATED="1519529799500" MODIFIED="1519530072633" LINK="https://en.wikipedia.org/wiki/Application_software"/>
+<node TEXT="&#x75c5;&#x6bd2;" ID="ID_810085649" CREATED="1519529819093" MODIFIED="1519529826815"/>
+</node>
+</node>
+</map>
